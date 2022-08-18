c6b7d8a13c432527534266384b47c221f0378108
gertcode <gertcode@gmail.com>
2022-08-17 19:11:38 -0500

    Servidor apollo basico

---
+++ courses.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {id:"1", title: "Curso de GraphQL", views: 1000},
+    {id:"2", title: "Curso de JavaScript2", views: 50000},
+    {id:"3", title: "Curso de JavaScript3", views: 50000},
+    {id:"4", title: "Curso de JavaScript4", views: 50000},
+    {id:"5", title: "Curso de JavaScript5", views: 50000},
+    {id:"6", title: "Curso de JavaScript6", views: 50000},
+    {id:"7", title: "Curso de JavaScript7", views: 50000},
+    {id:"8", title: "Curso de JavaScript8", views: 50000},
+    {id:"9", title: "Curso de JavaScript9", views: 50000},
+    {id:"10", title: "Curso de JavaScript10", views: 50000},
+    {id:"11", title: "Curso de JavaScript11", views: 50000},
+    {id:"12", title: "Curso de JavaScript12", views: 50000},
+    {id:"12", title: "Curso de JavaScript13", views: 50000}
+]
